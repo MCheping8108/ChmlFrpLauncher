f324d300e99dfffb5f9f7b0a608702bb89ded69f
chaoji233 <chaoji@chcat.cn>
2026-02-15 08:03:52 +0800

    refactor: 将毛玻璃效果样式移至独立文件

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/index.css";
 import "@/App.css";
+import "@/frosted-glass.css";
 import App from "@/App.tsx";
 import { Toaster } from "@/components/ui/sonner";
 
